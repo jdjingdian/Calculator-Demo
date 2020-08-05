2c73b17bd62a21e53aad73e40c569b98641b0d1f
Derek Jing <djing@theknotww.com>
2020-08-05 14:27:32 +0800

    Initial Commit

---
+++ Calculator Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator Demo
+//
+//  Created by djing on 2020/8/5.
+//  Copyright © 2020 djing. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
