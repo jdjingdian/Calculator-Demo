d1d51e5c20677c0c34b2e004544fa9bc9f283406
Derek Jing <djing@theknotww.com>
2020-08-05 16:27:45 +0800

    init with SwiftUI&&Combine demo

---
+++ Calculator Demo/CalculatorButtonItem.swift
@@ -0,0 +1,31 @@
+//
+//  CalculatorButtonItem.swift
+//  Calculator Demo
+//
+//  Created by djing on 2020/8/5.
+//  Copyright © 2020 djing. All rights reserved.
+//
+
+import Foundation
+
+enum CalculatorButtonItem{
+    enum Op: String{
+        case plus = "+"
+        case minus = "-"
+        case divide = "÷"
+        case multiply = "×"
+        case equal = "="
+    }
+    case digit(Int)
+    case dot
+    case op(Op)
+}
+extension CalculatorButtonItem : Hashable{
+    var title: String{
+        switch self{
+        case .digit(let value):return String(value)
+        case .dot: return "."
+        case .op(let op): return op.rawValue
+        }
+    }
+}
--- Calculator Demo/ContentView.swift
+++ Calculator Demo/ContentView.swift
@@ -10,12 +10,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 16){
+        Text("0").font(.system(size: 80))
+        CalulatorPad()
+        }
+    }
+}
+
+
+struct CalulatorPad: View {
+    let pad: [[CalculatorButtonItem]] = [
+        [.digit(7), .digit(8), .digit(9), .op(.plus)],
+        [.digit(4), .digit(5), .digit(6), .op(.minus)],
+        [.digit(1), .digit(2), .digit(3), .op(.multiply)],
+        [.digit(0), .dot ,.op(.divide), .op(.equal)]
+    ]
+    var body : some View {
+        VStack(spacing: 8){
+            ForEach(pad, id: \.self){ row in
+                CalculatorButtonRow(row: row)
+            }
+        }
+    }
+    
+}
+
+struct CalculatorButtonRow: View {
+    let row: [CalculatorButtonItem]
+    var body: some View {
+        HStack {
+            ForEach(row, id: \.self){item in
+                CalculatorButton(title: item.title){
+                    print("buttom: \(item.title)")
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CalculatorButton: View {
+    let fontSize: CGFloat = 68
+    let title: String
+    //    let backgroundColorName: String
+    let action: ()->Void
+    var body: some View {
+        Button(action: action){
+            Text(title)
+                .font(.system(size: fontSize))
+                .foregroundColor(.white)
+                .frame(width: 88, height: 88)
+                .background(Color.orange)
+                .cornerRadius(50)
+        }
     }
 }
